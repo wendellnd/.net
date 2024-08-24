535d58c753fd58784f7243eeb19ec7ef7073c0ec
wendellnd <wendelldourado.wn@gmail.com>
2024-08-24 12:17:21 -0300

    types

--- FiapHelloWorld/FiapHelloWorld/Program.cs
+++ FiapHelloWorld/FiapHelloWorld/Program.cs
@@ -11,6 +11,22 @@ namespace FiapHelloWorld
     {
         static void Main(string[] args)
         {
+            int value = 10;
+            double valueDouble = 10.5;
+            bool valueBool = true;
+            string name = "Fiap";
+            var n = "Fiap";
+
+            value += 1;
+            
+            Console.WriteLine(value);
+
+            string[] values = { "a", "b", "c" };
+
+            IList<string> list = new List<string>();
+            list.Add("Item")
+            list.Insert(1, "Inserted Item");
+
             var modelo = new HelloModelo();
 
             modelo.Hello();
