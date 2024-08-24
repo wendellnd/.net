e4a0d2f7a4fea19a49f48e18d0bfd3edbeda587d
wendellnd <wendelldourado.wn@gmail.com>
2024-08-23 22:35:27 -0300

    first commit

---
+++ FiapHelloWorld/FiapHelloWorld/Modelo/HelloModelo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapHelloWorld.Modelo
+{
+    internal class HelloModelo
+    {
+        public string Message = "Hello, World!";
+
+        public void Hello()
+        {
+            Console.WriteLine(Message);
+
+            Console.Read();
+        }
+    }
+}
---
+++ FiapHelloWorld/FiapHelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiapHelloWorld.Modelo;
+
+namespace FiapHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var modelo = new HelloModelo();
+
+            modelo.Hello();
+        }
+    }
+}
